d8210b5d42d0bb49c0b0953f380a2e8573409550
Damiano Cipriani <damiano.cipriani@engicam.com>
2020-11-11 11:17:19 +0100

    Improve image_flash.sh with partitions check

--- recipes-core/utility-application/image-flash/image_flash.sh
+++ recipes-core/utility-application/image-flash/image_flash.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ $(id -u) -ne 0 ]
 then
@@ -62,15 +62,34 @@ then
   exit 1
 fi
 
+echo ""
 echo "deleting old partitions"
+echo ""
 
-sgdisk -g -d 1 $1
-sgdisk -g -d 2 $1
-sgdisk -g -d 3 $1
-sgdisk -g -d 4 $1
-sgdisk -g -d 5 $1
+part1=`sgdisk -i 1 $1`
+if [[ $part1 != *"does not exist"* ]]; then
+  sgdisk -g -d 1 $1
+fi
+part2=`sgdisk -i 2 $1`
+if [[ $part2 != *"does not exist"* ]]; then
+  sgdisk -g -d 2 $1
+fi
+part3=`sgdisk -i 3 $1`
+if [[ $part3 != *"does not exist"* ]]; then
+  sgdisk -g -d 3 $1
+fi
+part4=`sgdisk -i 4 $1`
+if [[ $part4 != *"does not exist"* ]]; then
+  sgdisk -g -d 4 $1
+fi
+part5=`sgdisk -i 5 $1`
+if [[ $part5 != *"does not exist"* ]]; then
+  sgdisk -g -d 5 $1
+fi
 
+echo ""
 echo "creating partitions"
+echo ""
 
 sgdisk -n 1:64:7167 $1
 sgdisk -n 2:16384:24575 $1
@@ -81,7 +100,9 @@ sgdisk -c 4:boot $1
 sgdisk -c 5:rootfs $1
 sgdisk -u 5:614e0000-0000-4b53-8000-1d28000054a9 $1
 
+echo ""
 echo "writing image"
+echo ""
 
 dd if=${image_path}/idbloader.img of=$1 seek=64
 dd if=${image_path}/uboot.img of=$1 seek=16384
